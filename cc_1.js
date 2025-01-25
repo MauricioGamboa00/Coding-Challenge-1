6e5e0acb3628a9760eeb0ff6185287b4511a9584
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-01-25 15:16:32 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,8 @@
+// Task 1 Variables
+let employeeName = "Mary Sue"
+const employeeID = 321
+var isActive = true
+
+console.log("Employee Name:", employeeName, "Type:", typeof employeeName);
+console.log("Employee ID:", employeeID, "Type:", typeof employeeID);
+console.log("Is Active:", isActive, "Type:", typeof isActive)
