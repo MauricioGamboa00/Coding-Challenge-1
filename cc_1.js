aeec68aa5bbd53beba72c9a2895e2696930addb8
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-01-25 15:23:50 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,4 +5,13 @@ var isActive = true
 
 console.log("Employee Name:", employeeName, "Type:", typeof employeeName);
 console.log("Employee ID:", employeeID, "Type:", typeof employeeID);
-console.log("Is Active:", isActive, "Type:", typeof isActive)
+console.log("Is Active:", isActive, "Type:", typeof isActive);
+
+// Task 2 Primitive Data Types
+let productName = "Wirless Gaming Headset";
+const productPrice = 129.99;
+var isAvailable = true;
+
+console.log("Product Name:", productName, "Type:", typeof productName);
+console.log("Product Price:", productPrice, "Type:", typeof productPrice);
+console.log("Is Available:", isAvailable, "Type:", typeof isAvailable);
